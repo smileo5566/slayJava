cc7d4505828f3d0c5963bb578a3804b5f50489d8
smileo5566 <liang40607@livemail.tw>
2018-12-14 15:49:28 +0800

    Add files via upload

--- Battle1.java
+++ Battle1.java
@@ -9,9 +9,9 @@ import java.util.TimerTask;
 
 public class Battle1 extends JFrame{
     private Container cp;
-    private JPanel jpnN = new JPanel(new GridLayout(2,6,1,1));
-    private JPanel jpnC = new JPanel(new GridLayout(1,14,1,1));
-    private JPanel jpnS = new JPanel(new GridLayout(2,4,1,1));
+//    private JPanel jpnN = new JPanel(new GridLayout(2,6,1,1));
+//    private JPanel jpnC = new JPanel(new GridLayout(1,14,1,1));
+//    private JPanel jpnS = new JPanel(new GridLayout(2,4,1,1));
 
     private JMenuBar jMB = new JMenuBar();
     private JMenu jMYo = new JMenu("Yo!");
@@ -20,53 +20,22 @@ public class Battle1 extends JFrame{
     private JMenuItem jMIRe = new JMenuItem("Restart");
     private JMenuItem jMIClose = new JMenuItem("Close");
 //    畫面
-    private JLabel jlbHpchg1 = new JLabel();
-    private JLabel jlbHpchg2 = new JLabel();
-    private JLabel jlbBlank1 = new JLabel();
-    private JLabel jlbBlank2 = new JLabel();
-    private JLabel jlbBlank3 = new JLabel();
-    private JLabel jlbBlank4 = new JLabel();
+//    private JLabel jlbHpchg1 = new JLabel();
+//    private JLabel jlbHpchg2 = new JLabel();
+//    private JLabel jlbBlank1 = new JLabel();
+//    private JLabel jlbBlank2 = new JLabel();
+//    private JLabel jlbBlank3 = new JLabel();
+//    private JLabel jlbBlank4 = new JLabel();
 
-    private ImageIcon icon1 = new ImageIcon("img/pencilMan.png");
-    private ImageIcon icon2 = new ImageIcon("img/monsterTako.png");
-    private ImageIcon icon3 = new ImageIcon("img/pencilManCut.png");
-    private ImageIcon icon4 = new ImageIcon("img/pencilManCry.png");
-    private ImageIcon icon5 = new ImageIcon("img/pencilManCutCry.png");
-    private ImageIcon icon6 = new ImageIcon("img/monsterTakoDead.png");
-    private JLabel pencilManImg = new JLabel();
-    private JLabel monsterTakoImg = new JLabel();
-    private JLabel pencilManCutImg = new JLabel();
-    private JLabel pencilManCryImg = new JLabel();
-    private JLabel pencilManCutCryImg = new JLabel();
-    private JLabel monsterTakoDeadImg = new JLabel();
-    private JLabel jlbBlank5 = new JLabel();
-    private JLabel jlbBlank6 = new JLabel();
-    private JLabel jlbBlank7 = new JLabel();
-    private JLabel jlbBlank8 = new JLabel();
-
-    private JLabel jlbManHpX = new JLabel("100");
-    private JLabel jlbManLine = new JLabel("/");
-    private JLabel jlbManHp = new JLabel("100");
-    private JLabel jlbTakoHpX = new JLabel("50");
-    private JLabel jlbTakoLine = new JLabel("/");
-    private JLabel jlbTakoHp = new JLabel("50");
-    private JLabel jlbBlank9 = new JLabel();
-    private JLabel jlbBlank10 = new JLabel();
-    private JLabel jlbBlank11 = new JLabel();
-    private JLabel jlbBlank12= new JLabel();
-    private JLabel jlbBlank13= new JLabel();
-    private JLabel jlbBlank14 = new JLabel();
-    private JLabel jlbBlank15 = new JLabel();
-    private JLabel jlbBlank16 = new JLabel();
+    private ImageIcon iconAshe = new ImageIcon("img/神射.png");
+    private JLabel Ashe = new JLabel();
+    private ImageIcon iconLanBao = new ImageIcon("img/神射.png");
+    private JLabel LanBao = new JLabel();
 
     private JButton jbtSkill1 = new JButton("攻擊!");
     private JButton jbtSkill2 = new JButton("回復!");
     private JButton jbtSkill3 = new JButton("剪頭髮!");
     private JButton jbtSkill4 = new JButton("不開心!");
-    private JLabel jlbBlank17 = new JLabel();
-    private JLabel jlbBlank18 = new JLabel();
-    private JLabel jlbBlank19 = new JLabel();
-    private JLabel jlbBlank20 = new JLabel();
 
     Random rdm1 = new Random();
 
@@ -79,91 +48,15 @@ public class Battle1 extends JFrame{
         this.setDefaultLookAndFeelDecorated(true);
         this.setJMenuBar(jMB);
         cp =this.getContentPane();
-        cp.add(jpnN,BorderLayout.NORTH);
-        cp.add(jpnC,BorderLayout.CENTER);
-        cp.add(jpnS,BorderLayout.SOUTH);
-        jpnN.setBackground(Color.white);
-        jpnC.setBackground(Color.white);
-        jpnS.setBackground(Color.white);
+        cp.setLayout(null);
+        cp =this.getContentPane();
+
         jMB.add(jMYo);
         jMB.add(jMTool);
         jMB.add(jMAbout);
         jMYo.add(jMIRe);
         jMYo.add(jMIClose);
-        jpnN.add(jlbBlank1);
-        jpnN.add(jlbHpchg1);
-        jpnN.add(jlbBlank2);
-        jpnN.add(jlbBlank3);
-        jpnN.add(jlbHpchg2);
-        jpnN.add(jlbBlank4);
-        jpnN.add(jlbBlank5);
-        jpnN.add(pencilManImg);
-        jpnN.add(jlbBlank6);
-        jpnN.add(jlbBlank7);
-        jpnN.add(monsterTakoImg);
-        jpnN.add(jlbBlank8);
 
-        jpnC.add(jlbBlank9);
-        jpnC.add(jlbBlank10);
-        jpnC.add(jlbManHpX);
-        jpnC.add(jlbManLine);
-        jpnC.add(jlbManHp);
-        jpnC.add(jlbBlank11);
-        jpnC.add(jlbBlank12);
-        jpnC.add(jlbBlank13);
-        jpnC.add(jlbBlank14);
-        jpnC.add(jlbTakoHpX);
-        jpnC.add(jlbTakoLine);
-        jpnC.add(jlbTakoHp);
-        jpnC.add(jlbBlank15);
-        jpnC.add(jlbBlank16);
-
-
-        jpnS.add(jlbBlank17);
-        jpnS.add(jbtSkill1);
-        jpnS.add(jbtSkill2);
-        jpnS.add(jlbBlank18);
-        jpnS.add(jlbBlank19);
-        jpnS.add(jbtSkill3);
-        jpnS.add(jbtSkill4);
-        jpnS.add(jlbBlank20);
-
-        jlbHpchg1.setForeground(Color.red);
-        jlbHpchg2.setForeground(Color.red);
-
-
-
-
-//        火柴人圖片
-        Image imgP = icon1.getImage();
-        Image newImgP =imgP.getScaledInstance(109,187, Image.SCALE_SMOOTH);
-        icon1 = new ImageIcon(newImgP);
-        pencilManImg.setIcon(icon1);
-//        火柴人剪髮圖片
-        Image imgPCut = icon3.getImage();
-        Image newImgPCut =imgPCut.getScaledInstance(109,187, Image.SCALE_SMOOTH);
-        icon3 = new ImageIcon(newImgPCut);
-        pencilManCutImg.setIcon(icon3);
-//        火柴人哭哭圖片
-        Image imgPCry = icon4.getImage();
-        Image newImgPCry =imgPCry.getScaledInstance(109,187, Image.SCALE_SMOOTH);
-        icon4 = new ImageIcon(newImgPCry);
-        pencilManCryImg.setIcon(icon4);
-//        火柴人哭哭剪髮圖片
-        Image imgPCutCry = icon5.getImage();
-        Image newImgPCutCry =imgPCutCry.getScaledInstance(109,187, Image.SCALE_SMOOTH);
-        icon5 = new ImageIcon(newImgPCutCry);
-        pencilManCutCryImg.setIcon(icon5);
-//        章魚怪圖片
-        Image imgT = icon2.getImage();
-        Image newImgT =imgT.getScaledInstance(163,130, Image.SCALE_SMOOTH);
-        icon2 = new ImageIcon(newImgT);
-        monsterTakoImg.setIcon(icon2);
-//        章魚怪圖片
-        Image imgTD = icon6.getImage();
-        Image newImgTD =imgTD.getScaledInstance(163,130, Image.SCALE_SMOOTH);
-        icon6 = new ImageIcon(newImgTD);
-        monsterTakoDeadImg.setIcon(icon6);
 
 
 
@@ -186,102 +79,102 @@ public class Battle1 extends JFrame{
             }
         });
 
-        jbtSkill1.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                int x = rdm1.nextInt(10) + 10;
-                int y = rdm1.nextInt(10) + 5;
-                Timer timer1 = new Timer();
-                timer1.schedule(new TimerTask() {
-                    public void run() {
-                        jlbHpchg2.setText("-" + Integer.toString(x));
-                        if ((Integer.parseInt(jlbTakoHpX.getText()) - x) <= 0){
-                            jlbTakoHpX.setText("0");
-                            monsterTakoImg.setIcon(icon6);
-                            JOptionPane.showMessageDialog(getParent(), "Win!");
-                            Battle1.this.setVisible(false);
-
-
-                        }else jlbTakoHpX.setText(Integer.toString((Integer.parseInt(jlbTakoHpX.getText()) - x)));
-
-                        }
-
-                }, 1000);
-                    timer1.schedule(new TimerTask() {
-                        public void run() {
-                            jlbHpchg1.setText("-" + Integer.toString(y));
-                            jlbManHpX.setText(Integer.toString((Integer.parseInt(jlbManHpX.getText()) - y)));
-
-                        }
-                    }, 2000);
-                    timer1.schedule(new TimerTask() {
-                        public void run() {
-                            jlbHpchg2.setText(" ");
-                        }
-                    }, 3000);
-                    timer1.schedule(new TimerTask() {
-                        public void run() {
-                            jlbHpchg1.setText(" ");
-                        }
-                    }, 4000);
-                }
-
-
-
-
-
-
-        });
-        jbtSkill2.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int x = rdm1.nextInt(10) + 10;
-                int y = rdm1.nextInt(10) + 5;
-                Timer timer1 = new Timer();
-                timer1.schedule(new TimerTask() {
-                    public void run() {
-                        jlbHpchg1.setText("+" + Integer.toString(x));
-                        if ((Integer.parseInt(jlbManHpX.getText()) + x) >= 100){
-                            jlbManHpX.setText("100");
-                        }else {
-                            jlbManHpX.setText(Integer.toString((Integer.parseInt(jlbManHpX.getText()) + x)));
-                        }
-                    }
-
-                }, 1000);
-                timer1.schedule(new TimerTask() {
-                    public void run() {
-                        jlbHpchg1.setText("-" + Integer.toString(y));
-                        jlbManHpX.setText(Integer.toString((Integer.parseInt(jlbManHpX.getText()) - y)));
-
-                    }
-                }, 2000);
-                timer1.schedule(new TimerTask() {
-                    public void run() {
-                        jlbHpchg2.setText(" ");
-                    }
-                }, 3000);
-                timer1.schedule(new TimerTask() {
-                    public void run() {
-                        jlbHpchg1.setText(" ");
-                    }
-                }, 4000);
-
-            }
-        });
-        jbtSkill3.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    pencilManImg.setIcon(icon3);
-            }
-        });
-        jbtSkill4.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                pencilManImg.setIcon(icon4);
-            }
-        });
+//        jbtSkill1.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//                int x = rdm1.nextInt(10) + 10;
+//                int y = rdm1.nextInt(10) + 5;
+//                Timer timer1 = new Timer();
+//                timer1.schedule(new TimerTask() {
+//                    public void run() {
+//                        jlbHpchg2.setText("-" + Integer.toString(x));
+//                        if ((Integer.parseInt(jlbTakoHpX.getText()) - x) <= 0){
+//                            jlbTakoHpX.setText("0");
+//                            monsterTakoImg.setIcon(icon6);
+//                            JOptionPane.showMessageDialog(getParent(), "Win!");
+//                            Battle1.this.setVisible(false);
+//
+//
+//                        }else jlbTakoHpX.setText(Integer.toString((Integer.parseInt(jlbTakoHpX.getText()) - x)));
+//
+//                        }
+//
+//                }, 1000);
+//                    timer1.schedule(new TimerTask() {
+//                        public void run() {
+//                            jlbHpchg1.setText("-" + Integer.toString(y));
+//                            jlbManHpX.setText(Integer.toString((Integer.parseInt(jlbManHpX.getText()) - y)));
+//
+//                        }
+//                    }, 2000);
+//                    timer1.schedule(new TimerTask() {
+//                        public void run() {
+//                            jlbHpchg2.setText(" ");
+//                        }
+//                    }, 3000);
+//                    timer1.schedule(new TimerTask() {
+//                        public void run() {
+//                            jlbHpchg1.setText(" ");
+//                        }
+//                    }, 4000);
+//                }
+//
+//
+//
+//
+//
+//
+//        });
+//        jbtSkill2.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                int x = rdm1.nextInt(10) + 10;
+//                int y = rdm1.nextInt(10) + 5;
+//                Timer timer1 = new Timer();
+//                timer1.schedule(new TimerTask() {
+//                    public void run() {
+//                        jlbHpchg1.setText("+" + Integer.toString(x));
+//                        if ((Integer.parseInt(jlbManHpX.getText()) + x) >= 100){
+//                            jlbManHpX.setText("100");
+//                        }else {
+//                            jlbManHpX.setText(Integer.toString((Integer.parseInt(jlbManHpX.getText()) + x)));
+//                        }
+//                    }
+//
+//                }, 1000);
+//                timer1.schedule(new TimerTask() {
+//                    public void run() {
+//                        jlbHpchg1.setText("-" + Integer.toString(y));
+//                        jlbManHpX.setText(Integer.toString((Integer.parseInt(jlbManHpX.getText()) - y)));
+//
+//                    }
+//                }, 2000);
+//                timer1.schedule(new TimerTask() {
+//                    public void run() {
+//                        jlbHpchg2.setText(" ");
+//                    }
+//                }, 3000);
+//                timer1.schedule(new TimerTask() {
+//                    public void run() {
+//                        jlbHpchg1.setText(" ");
+//                    }
+//                }, 4000);
+//
+//            }
+//        });
+//        jbtSkill3.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                    pencilManImg.setIcon(icon3);
+//            }
+//        });
+//        jbtSkill4.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                pencilManImg.setIcon(icon4);
+//            }
+//        });
 
     }
 
--- MainFrame.java
+++ MainFrame.java
@@ -1,47 +1,70 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class MainFrame extends JFrame{
     private Container cp;
-    private ImageIcon icon1 = new ImageIcon("img/pencilMan.png");
-    private ImageIcon icon2 = new ImageIcon("img/slayTitle.JPG");
-    private JLabel pencilManImg = new JLabel();
-    private JLabel slayImg = new JLabel();
+    private ImageIcon icon1 = new ImageIcon("img/Login.jpg");
+    private ImageIcon icon2 = new ImageIcon("img/LoginBtn.png");
+    private ImageIcon icon2_1 = new ImageIcon("img/LoginBtn.png");
+    private ImageIcon icon3 = new ImageIcon("img/End.png");
+    private ImageIcon icon3_2 = new ImageIcon("img/End.png");
+    private JLabel LoginImg = new JLabel();
+    private JLabel LoginBtnImg = new JLabel();
+    private JLabel LoginBtn2Img = new JLabel();
+    private JLabel EndImg = new JLabel();
+    private JLabel End2Img = new JLabel();
     private JButton jbtnRun = new JButton("Run");
     private JButton jbtnExit = new JButton("Exit");
-    private JPanel jpnC = new JPanel();
-    private JPanel jpnS = new JPanel(new GridLayout(2,2,1,1));
-    private JPanel jpnN = new JPanel();
     public MainFrame(){
         init();
     }
     public void init(){
-        this.setBounds(675,250,300,500);
+        this.setBounds(675,250,800,600);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setDefaultLookAndFeelDecorated(true);
         cp =this.getContentPane();
-        cp.add(jpnN,BorderLayout.NORTH);
-        cp.add(jpnC,BorderLayout.CENTER);
-        cp.add(jpnS,BorderLayout.SOUTH);
+        cp.setLayout(null);
+        cp.add(LoginBtnImg);
+        cp.add(EndImg);
+        cp.add(LoginImg);
 
-        jpnN.setBackground(Color.white);
-        jpnC.setBackground(Color.white);
-        jpnS.setBackground(Color.white);
-        jpnN.add(pencilManImg);
-        jpnC.add(slayImg);
-        jpnS.add(jbtnRun);
-        jpnS.add(jbtnExit);
-//       火柴人圖片
-        Image imgP = icon1.getImage();
-        Image newImgP =imgP.getScaledInstance(109,187, Image.SCALE_SMOOTH);
-        icon1 = new ImageIcon(newImgP);
-        pencilManImg.setIcon(icon1);
-//        Slay圖片
-        Image imgS = icon2.getImage();
-        Image newImgS =imgS.getScaledInstance(235,130, Image.SCALE_SMOOTH);
-        icon2 = new ImageIcon(newImgS);
-        slayImg.setIcon(icon2);
+
+
+        LoginImg.setBounds(0,0,800,600);
+        LoginBtnImg.setBounds(600,237,89,42);
+        EndImg.setBounds(608,353,65,35);
+
+//       Login圖片
+        Image imgL = icon1.getImage();
+        Image newImgL =imgL.getScaledInstance(800,600, Image.SCALE_SMOOTH);
+        icon1 = new ImageIcon(newImgL);
+        LoginImg.setIcon(icon1);
+//       LoginBtn圖片
+        Image imgLB = icon2.getImage();
+        Image newImgLB =imgLB.getScaledInstance(81,38, Image.SCALE_SMOOTH);
+        icon2 = new ImageIcon(newImgLB);
+        LoginBtnImg.setIcon(icon2);
+
+        Image imgLB2 = icon2_1.getImage();
+        Image newImgLB2 =imgLB2.getScaledInstance(89,42, Image.SCALE_SMOOTH);
+        icon2_1 = new ImageIcon(newImgLB2);
+        LoginBtn2Img.setIcon(icon2_1);
+//        End圖片
+        Image imgE = icon3.getImage();
+        Image newImgE =imgE.getScaledInstance(89,42, Image.SCALE_SMOOTH);
+        icon3 = new ImageIcon(newImgE);
+        End2Img.setIcon(icon3);
+
+        Image imgE2 = icon3_2.getImage();
+        Image newImgE2 =imgE2.getScaledInstance(89,42, Image.SCALE_SMOOTH);
+        icon3_2 = new ImageIcon(newImgE2);
+        End2Img.setIcon(icon3_2);
 
 
         jbtnExit.addActionListener(new AbstractAction() {
@@ -59,5 +82,45 @@ public class MainFrame extends JFrame{
             }
         });
 
+        LoginBtnImg.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                Map map1 = new Map();
+                map1.setVisible(true);
+                MainFrame.this.setVisible(false);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                LoginBtnImg.setIcon(icon2_1);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                LoginBtnImg.setIcon(icon2);
+            }
+        });
+        EndImg.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.exit(0);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                LoginBtnImg.setIcon(icon2_1);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                LoginBtnImg.setIcon(icon2);
+            }
+        });
     }
 }
